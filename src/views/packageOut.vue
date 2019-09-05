adc5464574707f765f30260e84810186b79c453a
mr1ng <452180727@qq.com>
2019-09-05 23:34:47 +0800

    first

---
+++ src/views/packageOut.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>预约取件</h1>
+    <a-form>
+      <a-form-item label="运单号" :label-col="{span: 5}" :wrapper-col="{span: 12}">
+        <a-input
+          v-decorator="[
+          'note',
+          {rules: [{required: true, message: 'Please input your note!'}]}
+        ]"
+        />
+      </a-form-item>
+      <a-form-item label="取件时间" :label-col="{span: 5}" :wrapper-col="{span: 12}">
+        <a-input
+          v-decorator="[
+          'note',
+          {rules: [{required: true, message: 'Please input your note!'}]}
+        ]"
+        />
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "packageOut",
+  components: {
+    HelloWorld
+  }
+}
+</Script>
